4d3b026a8e1d0fdaa428e6de12d768a185d858f9
David Cardenas <davidcardenas14@yahoo.com>
2025-04-03 23:14:10 -0400

    finished multiple choice

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -2,14 +2,29 @@ import React, { useState } from "react";
 
 export function MultipleChoiceQuestion({
     options,
-    expectedAnswer,
+    expectedAnswer
 }: {
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selected, setSelected] = useState<string>(options[0]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select
+                value={selected}
+                onChange={(e) => {
+                    setSelected(e.target.value);
+                }}
+            >
+                {options.map((option: string) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <span>{selected === expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
